e3103bfc451e86853eb94069818a70fbb2a10400
Cheng Lin <bnn1044@gmail.com>
2017-07-28 10:09:10 -0400

    ADD pid control to the current mode

--- battery_tester/battery_tester.ino
+++ battery_tester/battery_tester.ino
@@ -3,11 +3,21 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Button.h>
+#include <SoftwareSerial.h>
+#include <PID_v1.h>
 //#define OLED_RESET 4
 Adafruit_SSD1306 display;
 
+SoftwareSerial LogSerial(3, 2); // RX, TX
+
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
+
 /**************Run options***************************/
-#define DEBUG
+//#define DEBUG
 
 
 #define VoltageSensePin        A0
@@ -16,23 +26,33 @@ Adafruit_SSD1306 display;
 
 /********************Button & Menu Variable*****************/
 
-Button ButtonBottomLeft  = Button(6,PULLUP);     //6
-Button ButtonBottomRight = Button(7,PULLUP);    //7
+Button Select  = Button(6,PULLUP);     //6
+Button Start   = Button(7,PULLUP);    //7
+Button Minus   = Button(8,PULLUP);        //8
+Button Plus    = Button(9,PULLUP);       //9
 
-Button ButtonTopLeft     = Button(8,PULLUP);        //8
-Button ButtonTopRight    = Button(9,PULLUP);       //9
-int MenuPage = 0;                             // 0 Mainpage   1 TestPage
-int CurrentStep = 100;                         //5A
-int VoltageChgStep = 10;                      // 0.1v
+int ChangeStep = 10;                         //5A
 /*Sensor variable************************/
 
-#define CurrnetSensorOffset    (1024*(0.5/5.0))  //default 0.5v
+#define CurrnetSensorOffset    (1023*(0.5/5.0))  //default 0.5v
 float CutOffVoltage = 1400;       //14.00V
-float CurrentSetPoint = 6000;     //60.00A
-float Voltage = 0.0;
-float Current = 0.0;
-float RefVoltage = 0.0;
-float   Temp    = 0;
+float CurrentSetPoint = 600;     //60.00A
+float TempsetPoint    =650;       //  temperature setpoint
+float Voltage = 0.0;              //voltage
+float Current = 0.0;              //current
+float RefVoltage = 0.0;           //reference voltage
+float Temp    = 0;                //Temperature
+long mah = 1300;                  //mAH
+long timeStamp = 0;               //time
+boolean TestStart = 0;            //Test Start; 0 stop or test done ,1 test start;
+int minites,seconds;              // minites Second
+long timetemp = 0;                // temperary Time save
+
+// Menu Items.
+#define setVoltage 0    
+#define setCurrent 1
+#define setTemp    2 
+int SettingItem = 0;   // 0 set voltage, 1 set current, 2 setTemp
 
 void setup()   {                
   Serial.begin(9600);
@@ -42,84 +62,145 @@ void setup()   {
   display.clearDisplay();
   delay(500);
   GetSensor();
-  StartupScreen();
+  myPID.SetMode(AUTOMATIC);
+}
+
+void doMath(){
+  
+  //calcultate the mah
+  //calculate the minites
+  //When start the time capture, need to do // timeStamp =millis(); first before calling 
+  //compute PID
+   Setpoint = CurrentSetPoint;
+   Input = Current;
+   myPID.Compute();
+   analogWrite( 5,Output);
+   printDebugMsg("Setpoint: ",Setpoint);
+   printDebugMsg("Input: ",Input);
+   printDebugMsg("OUTPUTPWM: ",Output);
 }
 void loop() {
   UpdateScreen();
-  ChangeSetPoint();
-}
-void ChangeSetPoint(){
-  if ( MenuPage ==0 ){
-    int CurrentChangeReq = CurrentSetPoint;
-    int VoltageSetPointReq = CutOffVoltage;
-    //voltage change
-    if ( ButtonTopLeft.isPressed()){
-      VoltageSetPointReq = VoltageSetPointReq - VoltageChgStep;
-    }
-    if ( ButtonTopRight.isPressed()  ){
-      VoltageSetPointReq = VoltageSetPointReq + VoltageChgStep;
-    }
-    CutOffVoltage  = VoltageSetPointReq;
-    // current change
-    if ( ButtonBottomLeft.isPressed() ){
-      CurrentChangeReq = CurrentChangeReq - CurrentStep;
-    }
-    if ( ButtonBottomRight.isPressed() ){
-      CurrentChangeReq = CurrentChangeReq + CurrentStep;
-    }
-      CurrentSetPoint  = CurrentChangeReq;
+  GetSensor();
+  GetSetPoint();
+  if ( GetStartKey()){             //test start
+      timetemp = millis();
+      timeStamp = 0;
+      printDebugMsg("Test start ",TestStart);
+      while( TestStart ){
+          timeStamp =millis()- timetemp;
+          minites = ( timeStamp/1000 ) /60;
+          seconds = ( timeStamp/1000 ) % 60;    // get the uptime
+          GetSensor();            //read the data
+          UpdateScreen();         //Update screen
+          doMath();
+         // CheckBatteryStutas();
+      }
   }
+}
+boolean CheckBatteryStutas(){
+  if ( Voltage < CutOffVoltage ){               // Voltage < cut off voltage
+      TestStart = 0;
+  }
+  if ( Temp > setTemp ){                        // over temp
+      TestStart = 0;
+  }
+  return TestStart;
+}
+boolean GetStartKey(){
+  if ( Start.uniquePress()){
+    if ( !TestStart ){
+      TestStart = 1;
+    }else
+      TestStart = 0;
+  }
+  return TestStart;
+}
+void GetSetPoint(){
+  int CurrentChangeReq = CurrentSetPoint;
+  int VoltageSetPointReq = CutOffVoltage;
+  int TemperaterReq      = TempsetPoint ;
+   if ( Select.uniquePress()){
+        SettingItem = SettingItem+1;
+        if ( SettingItem >= 3 ){
+           SettingItem = 0;
+        }
+  }
+    //voltage change
+    if ( Minus.isPressed()){
+      switch ( SettingItem ){
+        case 0 : 
+          VoltageSetPointReq = VoltageSetPointReq - ChangeStep;
+          CutOffVoltage  = VoltageSetPointReq;
+          break;
+        case 1:
+          CurrentChangeReq = CurrentChangeReq - ChangeStep;
+          CurrentSetPoint = CurrentChangeReq;
+          break;
+        case 2:
+          TemperaterReq = TemperaterReq - ChangeStep;
+          TempsetPoint = TemperaterReq;
+          break;
+      }
+    }
+    if ( Plus.isPressed()){
+       switch ( SettingItem ){
+        case 0 : 
+          VoltageSetPointReq = VoltageSetPointReq + ChangeStep;
+          CutOffVoltage  = VoltageSetPointReq;
+          break;
+        case 1:
+          CurrentChangeReq = CurrentChangeReq + ChangeStep;
+          CurrentSetPoint = CurrentChangeReq;
+          break;
+        case 2:
+          TemperaterReq = TemperaterReq + ChangeStep;
+          TempsetPoint = TemperaterReq;
+          break;
+      }
+    }
 }
 void UpdateScreen(){
   display.clearDisplay();
-  StartupScreen();
+  TestScreen();
   display.display();
 }
 void TestScreen(){
-    MenuPage = 1;                                    // in test page;
-    displayChar(10,0,"BAT   SET",2);                //5x8*2  finish at col 16
-    displayNumber(0,16,Voltage,2);                    //Bat Voltage
-    displayChar(61,24,"V",1);                    //Bat Voltage
-    displayNumber(68,16,(CutOffVoltage/100),2);   //Set voltage
+  /*12.00 v 14.00
+   *45.0  A 40.0
+   *25.0  C 65.0
+   *10000 mah 00:00
+   */
+    displayNumber(0,0,Voltage,2,2);                    //Bat Voltage
+    displayChar(60,8,"V",1);                         //Bat Voltage
+    displayNumber(68,0,(CutOffVoltage/100),2,2);      //Set voltage
+    if ( SettingItem == setVoltage ){
+      display.drawRect(68,0,60,16,1);
+    }
+    //display.setTextWrap(0);
     // display Current
-    displayNumber(0,32,Current,2);                    //Bat Voltage
-    displayChar(61,36,"A",1);                    //Bat Voltage/
-    displayNumber(68,32,(CurrentSetPoint/100),2);   //Set voltage
+    displayNumber(0,16,Current,2,1);                    //Bat current
+    displayChar(60,23,"A",1);                           //Bat current
+    displayNumber(72,16,(CurrentSetPoint/10),2,1);      //Set current
+    if ( SettingItem == setCurrent ){
+      display.drawRect(70,15,50,17,1);
+    }
     // display the temp
-    display.setCursor(0,48);
-    display.println(Temp,1);
-    displayChar(45,48,"c",2);
-    
+    displayNumber(0,33,Temp,2,1);                       //temp
+    displayChar(60,40,"C",1);                           //temp
+    displayNumber(72,33,(TempsetPoint/10),2,1);         //Set set temp
+    if ( SettingItem == setTemp ){
+      display.drawRect(70,31,50,17,1);
+    }
+    // display mah
+    displayNumber(0,50,mah,2,0);                         //mah
+    //display time
+    if ( minites>0 ){
+      displayNumber(64,50,minites,2,0);                   //display mintues
+      displayChar(85,50,":",2);
+    }
+    displayNumber(95,50,seconds,2,0);                   //display mintues
 }
-void StartupScreen(){
-/*  -BAT---SET- *    
- *  V12.000v    
-    A100.00A
-    **Temp :12.00C
-         START   
-*/  
-    MenuPage = 0;                                    // in main page;
-  //  display.clearDisplay();
-    displayChar(10,0,"BAT   SET",2);                //5x8*2  finish at col 16
-    // display Voltage
-    displayNumber(0,16,Voltage,2);                    //Bat Voltage
-    displayChar(61,24,"V",1);                    //Bat Voltage
-    displayNumber(68,16,(CutOffVoltage/100),2);   //Set voltage
-    // display Current
-    displayNumber(0,32,Current,2);                    //Bat Voltage
-    displayChar(61,36,"A",1);                    //Bat Voltage/
-    displayNumber(68,32,(CurrentSetPoint/100),2);   //Set voltage
-    // display the temp
-    display.setCursor(0,48);
-    display.println(Temp,1);
-    displayChar(45,48,"c",2);
-#ifdef DEBUG
-    displayNumber(80,56,RefVoltage,1);
-#endif
-  //  display.display();
-}
-//Size 2 can diplay 4 line . each can display 10 numbers
-//
 void displayChar( int x, int y,char *Char,int Size ){
   display.setTextSize(Size);
   display.setTextColor(WHITE);
@@ -127,18 +208,21 @@ void displayChar( int x, int y,char *Char,int Size ){
   display.println(Char);
   //display.display();
 }
-void displayNumber(int x, int y, float Number,int Size){
+void displayNumber(int x, int y, float Number,int Size,int decPoint){
  // String temp;
   display.setTextSize(Size);
   display.setTextColor(WHITE);
   display.setCursor(x,y);
-  display.print(Number);
+  display.println(Number,decPoint);
   //display.display();
 }
 void GetSensor(){
   long V = readADC(VoltageSensePin);  // get voltage
   V = map(V,0,1023,0,32000);          //0-32v
   Voltage = V/1000;
+#ifdef DEBUG
+  printDebugMsg("Voltage",Voltage);
+#endif  
   long A = readADC(CurrentSensePin);  // get current
   if ( A < CurrnetSensorOffset){
     A = CurrnetSensorOffset;
@@ -147,15 +231,15 @@ void GetSensor(){
   Current = A/100;
   long X = readADC(VoltageReference);
   RefVoltage = map(X,0,1023,0,4740);          //0-100A
+#ifdef DEBUG  
   printDebugMsg("Refence: ",RefVoltage/1000);
+#endif  
 }
-
 void printDebugMsg(String Temp, uint16_t number){
   String temp1;
   temp1 = Temp + number;
   Serial.println(temp1);
 }
-
 
 // read values
 // to average
