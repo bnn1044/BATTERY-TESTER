2368920b4b158b08edf1e15d8638afd319060c53
Cheng Lin <bnn1044@gmail.com>
2017-08-04 21:42:00 -0400

    addd

--- battery_tester/battery_tester.ino
+++ battery_tester/battery_tester.ino
@@ -1,34 +1,35 @@
-#include <SPI.h>
-#include <Wire.h>
-//#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+
 #include <Button.h>
 #include <SoftwareSerial.h>
-//#include <PID_v1.h>
+#include <PID_v1.h>
 
-//#include <OneWire.h>
-//#include <DallasTemperature.h>
+#define I2C_ADDRESS 0x3C
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiAvrI2c.h"
+SSD1306AsciiAvrI2c display;
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
  
 // Data wire is plugged into pin 4 on the Arduino
-//#define ONE_WIRE_BUS 3
+#define ONE_WIRE_BUS 3
 
-//OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
-//DallasTemperature TempSensor(&oneWire);
- 
-Adafruit_SSD1306 display;
+DallasTemperature TempSensor(&oneWire);
 
 SoftwareSerial LogSerial(3, 2); // RX, TX
 
 //Specify the links and initial tuning parameters
 double Setpoint, Input, Output;
-//PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
+PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
 /**************Run options***************************/
 //#define DEBUG
 #define VoltageSensePin        A0
 #define CurrentSensePin        A1
 #define VoltageReference       A2
 #define PWMoutputPin           5
+
 /********************Button & Menu Variable*****************/
 
 Button Select  = Button(6,PULLUP);    //6
@@ -59,18 +60,19 @@ long timetemp = 0;                // temperary Time save
 #define setTemp    2 
 int SettingItem = 0;   // 0 set voltage, 1 set current, 2 setTemp
 
-void setup()   {                
+void setup()   {     
+
+  display.begin(&Adafruit128x64, I2C_ADDRESS);
+  display.setFont(System5x7);
+  display.clear();
+  display.print("Hello world!");
+             
   Serial.begin(9600);
- // LogSerial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
-  display.display();
-  delay(200);
-  display.clearDisplay();
-  delay(500);
-// TempSensor.begin();
+  LogSerial.begin(9600);
+ TempSensor.begin();
+ 
   GetSensor();
-  LogSerial.println("test print on the SD card");
- // myPID.SetMode(AUTOMATIC);
+  myPID.SetMode(AUTOMATIC);
 }
 
 void doMath(){
@@ -88,9 +90,9 @@ void doMath(){
    printDebugMsg("OUTPUTPWM: ",Output);
 }
 void loop() {
-  UpdateScreen();
   GetSensor();
   GetSetPoint();
+   TestScreen();
   if ( GetStartKey()){             //test start
       timetemp = millis();
       timeStamp = 0;
@@ -100,7 +102,6 @@ void loop() {
           minites = ( timeStamp/1000 ) /60;
           seconds = ( timeStamp/1000 ) % 60;    // get the uptime
           GetSensor();            //read the data
-          UpdateScreen();         //Update screen
           doMath();
          //CheckBatteryStutas();
       }
@@ -168,11 +169,6 @@ void GetSetPoint(){
       }
     }
 }
-void UpdateScreen(){
-  display.clearDisplay();
-  TestScreen();
-  display.display();
-}
 void TestScreen(){
   /*12.00 v 14.00
    *45.0  A 40.0
@@ -182,24 +178,24 @@ void TestScreen(){
     displayNumber(0,0,Voltage,2,2);                    //Bat Voltage
     displayChar(60,8,"V",1);                         //Bat Voltage
     displayNumber(68,0,(CutOffVoltage/100.0),2,2);      //Set voltage
-    if ( SettingItem == setVoltage ){
+   /* if ( SettingItem == setVoltage ){
       display.drawRect(68,0,60,16,1);
-    }
+    }*/
     //display.setTextWrap(0);
     // display Current
     displayNumber(0,16,Current,2,1);                    //Bat current
     displayChar(60,23,"A",1);                           //Bat current
     displayNumber(72,16,(CurrentSetPoint/10.0),2,1);      //Set current
-    if ( SettingItem == setCurrent ){
+   /* if ( SettingItem == setCurrent ){
       display.drawRect(70,15,50,17,1);
-    }
+    }*/
     // display the temp
     displayNumber(0,33,Temp,2,1);                       //temp
     displayChar(60,40,"C",1);                           //temp
     displayNumber(72,33,(TempsetPoint/10.0),2,1);         //Set set temp
-    if ( SettingItem == setTemp ){
+   /* if ( SettingItem == setTemp ){
       display.drawRect(70,31,50,17,1);
-    }
+    }*/
     // display mah
     displayNumber(0,50,mah,2,0);                         //mah
     //display time
@@ -210,14 +206,21 @@ void TestScreen(){
     displayNumber(95,50,seconds,2,0);                   //display mintues
 }
 void displayChar( int x, int y,char *Char,int Size ){
-  display.setTextSize(Size);
-  display.setTextColor(WHITE);
+  //display.setTextSize(Size);
+  if ( Size == 1){
+    display.set1X();
+  }else  
+  display.set1X();
+  
   display.setCursor(x,y);
   display.println(Char);
 }
 void displayNumber(int x, int y, float Number,int Size,int decPoint){
-  display.setTextSize(Size);
-  display.setTextColor(WHITE);
+  if ( Size == 1){
+    display.set1X();
+  }
+  else 
+  display.set2X();
   display.setCursor(x,y);
   display.println(Number,decPoint);
 }
